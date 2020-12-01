bc9ce6aa3bafe2787d0c9d777c9bd03877d1d03d
yuukioda999 <oda.yuuki@acrovision.jp>
2020-12-01 12:48:45 +0900

    php

--- php-library/register.php
+++ php-library/register.php
@@ -1,17 +1,3 @@
-head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-  <body>
-    <div class="main">
-      <h1>書籍</h1>
-      <form></form>
-    </div>
-    
-  
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php 
+
+echo 'hello php!!!';
