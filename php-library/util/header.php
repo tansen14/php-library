19e9e1c28e54e36d9606e279d59197e31c7dcf89
yuka <yk15.e.oras2@icloud.com>
2020-12-03 09:54:26 +0900

    headerを追加

--- php-library/util/header.php
+++ php-library/util/header.php
@@ -1,10 +1,40 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<style>
+  .navbar-brand {
+    background: url("https://user-images.githubusercontent.com/70254894/100948522-5d5b0e80-354b-11eb-9a4b-82e0ebb5ef42.png") no-repeat left center;
+    background-size: contain;
+    height: 70px;
+    width: 70px;
+  }
 
-  </body>
-</html>
+  @media screen and (min-width: 576px) {
+    li+ li {
+      border-left: 1px solid #BBBBBB;
+    }
+   }
+</style>
+
+<nav class="navbar navbar-expand-sm navbar-light">
+  <a class="navbar-brand" href="#" class="rounded-circle"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#home">　Home　</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">　About　</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#myPage">　My Page　</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#signup">　Sign up　</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#logout">　Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
